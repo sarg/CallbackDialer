78a940d5d024789a66d8ef830de42cc3e4ea0004
Sergey Trofimov <sarg@sarg.org.ru>
2010-11-06 20:58:20 +0300

    use ImageButton for widget

--- src/com/sarg/dialer/Widget.java
+++ src/com/sarg/dialer/Widget.java
@@ -7,7 +7,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
 
@@ -22,16 +21,14 @@ public class Widget extends AppWidgetProvider {
 	
 	@Override
 	public void onDisabled(Context context) {
-		PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean("enabled", false);
-		
-		super.onDisabled(context);
+		PreferenceManager.getDefaultSharedPreferences(context).edit()
+			.putBoolean("enabled", false).commit();
 	}
 
 	@Override
 	public void onEnabled(Context context) {
-		PreferenceManager.getDefaultSharedPreferences(context).edit().putBoolean("enabled", false);
-		
-		super.onEnabled(context);
+		PreferenceManager.getDefaultSharedPreferences(context).edit()
+			.putBoolean("enabled", false).commit();
 	}
 
 	static void updateWidget(Context context, AppWidgetManager appWidgetManager,
@@ -39,16 +36,19 @@ public class Widget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        views.setTextViewText(R.id.widget_button, prefs.getBoolean("enabled", false) ? "ON" : "OFF");
+        
+        views.setImageViewResource(R.id.widget_button, 
+        		prefs.getBoolean("enabled", false) 
+        			? R.drawable.icon
+        			: R.drawable.icon_off
+        );
         
 		Intent intent = new Intent(context, Widget.class);
 		intent.addCategory(Intent.CATEGORY_ALTERNATIVE);
 
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-		
 		views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
 
-		// Tell the widget manager
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -59,9 +59,9 @@ public class Widget extends AppWidgetProvider {
 		if (intent.hasCategory(Intent.CATEGORY_ALTERNATIVE)) {
 			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 			
-			Editor editor = prefs.edit(); 
-			editor.putBoolean("enabled", ! prefs.getBoolean("enabled", true));
-			editor.commit();
+			prefs.edit()
+				.putBoolean("enabled", ! prefs.getBoolean("enabled", true))
+				.commit();
 
 			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 			ComponentName thisAppWidget = new ComponentName(context.getPackageName(), Widget.class.getName());
